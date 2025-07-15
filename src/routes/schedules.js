251724acb64a4aab65ebfa7f09c5db39ecbbe079
Kajitani Ayano <211447794+a-kajitani@users.noreply.github.com>
2025-07-15 13:23:24 +0900

    typo修正

--- src/routes/schedules.js
+++ src/routes/schedules.js
@@ -13,7 +13,6 @@ const app = new Hono();
 
 app.use(ensureAuthenticated());
 
-
 const scheduleIdValidator = zValidator(
   'param',
   z.object({
@@ -286,7 +285,7 @@ function isMine(userId, schedule) {
   return schedule && parseInt(schedule.createdBy, 10) === parseInt(userId, 10);
 }
 
-app.get('/:scheduleId/edit', async (c) => {
+app.get('/:scheduleId/edit',scheduleIdValidator, async (c) => {
   const { user } = c.get('session') ?? {};
   const schedule = await prisma.schedule.findUnique({
     where: { scheduleId: c.req.valid('param').scheduleId },
